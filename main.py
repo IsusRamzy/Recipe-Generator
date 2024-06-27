d348ae4b35b16da78417f8d14f4466cca50a9b35
Isus Ramzy <128253026+IsusRamzy@users.noreply.github.com>
2024-06-27 22:37:07 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+import google.generativeai as genai
+
+app = Flask(__name__)
+genai.configure(api_key='REPLACE WITH YOUR API KEY')
+model = genai.GenerativeModel('gemini-1.5-pro')
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+  if request.method === 'GET':
+    return render_template('index.html')
+  else:
+    prompt = request.form['prompt']
+    recipe = model.generate_content(prompt)
+    return render_template('index.html', recipe = recipe)    
+
+if __name__ == '__main__': # Not imported, which means not WSGI
+  app.run(debug=True)

9eecf9fcc2b6060452d263cc0d00a109361625a9
Isus Ramzy <128253026+IsusRamzy@users.noreply.github.com>
2024-06-27 23:06:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template, request
 import google.generativeai as genai
+import markdown
 
 app = Flask(__name__)
-genai.configure(api_key='REPLACE WITH YOUR API KEY')
-model = genai.GenerativeModel('gemini-1.5-pro')
+genai.configure(api_key='API KEY')
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+model = genai.GenerativeModel('gemini-1.5-pro', generation_config=generation_config, system_instruction="You are the `recipe generator`.")
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -11,8 +19,8 @@ def main():
     return render_template('index.html')
   else:
     prompt = request.form['prompt']
-    recipe = model.generate_content(prompt)
-    return render_template('index.html', recipe = recipe)    
+    recipe = model.generate_content(prompt).text
+    return render_template('index.html', recipe = markdown.markdown(recipe))    
 
 if __name__ == '__main__': # Not imported, which means not WSGI
-  app.run(debug=True)
+  app.run(debug=True)# 
